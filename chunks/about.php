50cd522d6d20727b695767f8355f3b19aa368c4a
Janhavi-pardeshi <145195914+Janhavi-pardeshi@users.noreply.github.com>
2024-07-26 21:11:03 +0530

    Add files via upload

---
+++ chunks/about.php
@@ -0,0 +1,8 @@
+<section class="fabout">
+		<div class="section white center">
+		      <div class="row container">
+		        <h2 class="header">ABOUT US</h2>
+		        <p class="grey-text text-darken-3 lighten-3">Welcome to RESTRO, where culinary excellence meets warm hospitality. Our story is one of passion, dedication, and a commitment to delivering an exceptional dining experience to our cherished guests. We believe in the artistry of food. Our culinary team, led by RUSHI, is dedicated to crafting dishes that tantalize the taste buds and celebrate the finest ingredients. From farm-fresh produce to premium cuts of meat, we source only the best to ensure every bite is a culinary delight. We are proud to be an active part of the College Road community. Through various initiatives, we support local causes, contribute to charitable events, and collaborate with neighboring businesses. It's our way of giving back to the community that has embraced us so warmly. Your Feedback Matters...</p>
+				<a href="/RESTRO/about-RESTRO.php" class="waves-effect waves-light btn" style="background: #ee6e73 !important;">Read More &raquo;</a>
+		      </div>
+	</section>
---
+++ chunks/description.php
@@ -0,0 +1,13 @@
+<section class="fdes">
+		<div class="section white center">
+			<h2 class="header" style="padding:20px; padding-bottom: 30px;">Resturant Powered By RESTRO</h2>
+		      <div class="row container center">
+		        <div class="col center l8 s12">
+		        	<p>RESTRO is not just a culinary destination; it's a commitment to a mindful dining experience. Passionately dedicated to sourcing local, sustainable, and organic ingredients, every dish at RESTRO is a celebration of freshness and flavor. With a culinary ethos centered on crafting each meal from scratch, infused with genuine love, the restaurant transcends traditional dining. Beyond the plate, RESTRO prioritizes social responsibility, ensuring their employees receive living wages, fostering a supportive work environment. Moreover, their dedication extends to environmental consciousness, striving to minimize their footprint. RESTRO is a haven where gastronomy meets ethics, creating an unforgettable and conscientious dining journey.</p>
+		        </div>
+		        <div class="col center l4 s12">
+		        	<img height="300" width="auto" style="object-fit: contain;" src="images/chef.jpg" alt="">
+		        </div>
+		        
+		      </div>
+	</section>
